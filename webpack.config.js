74e54601d3c3a8af2c186012891e6802e22b344b
root <root@MarcoL.localdomain>
2025-09-15 02:48:03 -0300

    spa version 1

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const copyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'main.js'
+    },
+    resolve: {
+        extensions: ['.js'],
+    },
+    module: {
+        rules: [{
+            test: /\.m?js$/, // Detecta archivos .js y también .mjs
+            exclude: /node_modules/,
+            use: {
+                loader: 'babel-loader'
+            }
+        }]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            inject: true,
+            template: './public/index.html',
+            filename: './index.html'
+        }),
+
+        new copyWebpackPlugin({
+        patterns: [{ from: './src/styles/styles.css',
+        to: '' }],
+        })
+    ]
+};
